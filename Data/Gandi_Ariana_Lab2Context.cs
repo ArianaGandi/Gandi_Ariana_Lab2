db58ad19e79b1b53b200627ebd265e77f5dfa60d
User <User@DESKTOP-QA4EKIJ>
2022-10-20 11:21:26 +0300

    Add project files.

---
+++ Data/Gandi_Ariana_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gandi_Ariana_Lab2.Models;
+
+namespace Gandi_Ariana_Lab2.Data
+{
+    public class Gandi_Ariana_Lab2Context : DbContext
+    {
+        public Gandi_Ariana_Lab2Context (DbContextOptions<Gandi_Ariana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gandi_Ariana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Gandi_Ariana_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Gandi_Ariana_Lab2.Models.Author> Author { get; set; }
+    }
+}
